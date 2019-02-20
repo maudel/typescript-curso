bf465a0c6230f2ffdf59a00230ce09846622005a
maudel <maudelaquintana@gmail.com>
2019-02-20 14:38:16 -0400

    primer commit

---
+++ curso.js
@@ -0,0 +1,6 @@
+function greet(name) {
+    return "Hello, " + name;
+}
+var user = "Typescript";
+document.body.innerHTML = greet(user);
+console.log('hola typescript');
---
+++ curso.ts
@@ -0,0 +1,7 @@
+function greet(name) {
+    return "Hello, " + name;
+}
+
+let user = "Typescript";
+
+document.body.innerHTML = greet(user);console.log('hola typescript');
